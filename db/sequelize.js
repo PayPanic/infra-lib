335569d7767d5304ca72f0345ba00b7d640e8d06
i586592 <snir.taoub@sap.com>
2025-07-18 14:14:31 +0300

    adding defaults

--- db/sequelize.js
+++ db/sequelize.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 function createSequelizeInstance({
-                                     dbName = process.env.DB_NAME,
-                                     dbUser = process.env.DB_USER,
-                                     dbPass = process.env.DB_PASS,
-                                     dbHost = process.env.DB_HOST,
+                                     dbName = process.env.DB_NAME || 'payments',
+                                     dbUser = process.env.DB_USER || 'postgres',
+                                     dbPass = process.env.DB_PASS || 'postgres',
+                                     dbHost = process.env.DB_HOST || 'localhost',
                                      dbPort = process.env.DB_PORT || 5432,
-                                 }) {
+                                 } = {}) {
     return new Sequelize(dbName, dbUser, dbPass, {
         host: dbHost,
         port: dbPort,
