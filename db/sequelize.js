fb5cecec050a3fcd6ad2457da8914808502edde6
i586592 <snir.taoub@sap.com>
2025-07-18 14:06:54 +0300

    adjusting kafka client

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,19 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
+function createSequelizeInstance({
+                                     dbName = process.env.DB_NAME,
+                                     dbUser = process.env.DB_USER,
+                                     dbPass = process.env.DB_PASS,
+                                     dbHost = process.env.DB_HOST,
+                                     dbPort = process.env.DB_PORT || 5432,
+                                 }) {
+    return new Sequelize(dbName, dbUser, dbPass, {
+        host: dbHost,
+        port: dbPort,
         dialect: 'postgres',
         logging: false,
-    }
-);
+    });
+}
 
-module.exports = sequelize;
+module.exports = { createSequelizeInstance };
